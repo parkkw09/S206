9619f98a18bca3fdc42aa43d3b6f2593c5a1c03a
parkkw09 <parkkw09@gmail.com>
2021-12-06 13:59:31 +0900

    Modified very simple content.

---
+++ S206/Domain/Mappers/S206Mapper.swift
@@ -0,0 +1,8 @@
+//
+//  S206Mapper.swift
+//  S206
+//
+//  Created by 박관웅 [parkkw09] on 2021/12/06.
+//
+
+import Foundation
---
+++ S206/Presentation/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S206
+//
+//  Created by 박관웅 [parkkw09] on 2021/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
